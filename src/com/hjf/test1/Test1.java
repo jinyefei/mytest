7a4e8d9296c9c1792b7aa0ad36b7065b46be520a
hujianfei-mac <jinyefei@aliyun.com>
2018-04-15 23:37:59 +0800

    review

---
+++ src/com/hjf/test1/Test1.java
@@ -0,0 +1,19 @@
+package com.hjf.test1;
+
+import java.util.Date;
+
+/**
+ * create by HJF
+ * 2018/4/15 19:43
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        byte b1 = 3,b2 = 4,b;
+        b = (byte) (b1+ b2);
+        b = 3 + 4;
+        b= (byte) 128;
+        float f1= 3.14f;
+
+        System.out.println(b);
+    }
+}
